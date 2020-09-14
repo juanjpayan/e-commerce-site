151deca79d10876b81011f3c446b579bb593d3da
jpjuansito <jpjuansito@penguin>
2020-09-13 23:51:20 -0400

    added form

---
+++ src/Components/Custom-Button/Custom-Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Custom-Button.scss'
+
+const CustomButton = ({children, ...otherProps}) => {
+    return (
+        <>
+        <button className="custom-button" {...otherProps}>
+            {children}
+        </button>
+        </>
+    )
+}
+
+export default CustomButton
---
+++ src/Pages/SignIn-SignUp/signIn- signUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../Components/Sign-In/Sign-In';
+import './signIn-signUp.scss';
+
+const signInAndSignUp = () => {
+	return (
+		<div className="sign-in-and-sign-up">
+			<SignIn />
+		</div>
+	);
+};
+
+export default signInAndSignUp;
