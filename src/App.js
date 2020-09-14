8fca8e2448db10aa8fa3566531230f262e0ec5e5
jpjuansito <jpjuansito@penguin>
2020-09-13 20:25:46 -0400

    added more components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
 import Homepage from './Pages/Homepage/homepage.component';
 import ShopPage from './Pages/Shop/shop.component';
 
 function App() {
 	return (
 		<div>
+			<Header />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route path="/shop" component={ShopPage} />
