7f04895b7d6d5850dadff6856c3d6bdb7c1cc65d
jpjuansito <jpjuansito@penguin>
2020-09-15 22:49:45 -0400

    added google signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { auth } from './Firebase/Firebase.utils';
 import './App.css';
 import Header from './Components/Header/Header';
 import Homepage from './Pages/Homepage/homepage.component';
@@ -7,15 +8,28 @@ import ShopPage from './Pages/Shop/shop.component';
 import signInAndSignUp from './Pages/SignIn-SignUp/signIn- signUp';
 
 function App() {
+	const [ currentUser, setCurrentUser ] = useState(null);
+
+	useEffect(() => {
+		const unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+			setCurrentUser(user);
+
+			console.log(user)
+		});
+
+		return () => {
+			unsubscribeFromAuth();
+		};
+	}, []);
 	return (
-		<div>
-			<Header />
+		<>
+			<Header currentUser={currentUser} />
 			<Switch>
 				<Route exact path="/" component={Homepage} />
 				<Route path="/shop" component={ShopPage} />
 				<Route path="/signin" component={signInAndSignUp} />
 			</Switch>
-		</div>
+		</>
 	);
 }
 
