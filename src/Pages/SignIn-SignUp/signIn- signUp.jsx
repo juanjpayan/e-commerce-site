61c595c22f9ad2d8666cf979ef5ac464a0618a44
jpjuansito <jpjuansito@penguin>
2020-09-17 23:23:49 -0400

    sign in and sign up

--- src/Pages/SignIn-SignUp/signIn- signUp.jsx
+++ src/Pages/SignIn-SignUp/signIn- signUp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SignIn from '../../Components/Sign-In/Sign-In';
+import SignUp from '../../Components/Sign-Up/Sing-Up';
 import './signIn-signUp.scss';
 
 const signInAndSignUp = () => {
 	return (
 		<div className="sign-in-and-sign-up">
 			<SignIn />
+			<SignUp />
 		</div>
 	);
 };
